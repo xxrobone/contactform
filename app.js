f12a1599531558a8a0db1db879d38b37fb5f9325
robonexx <robertwagar@gmail.com>
2023-01-27 07:30:32 +0100

    I will try to animate it all myself, but might use anime.js :D

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const mailBtn = document.querySelector('.mail_btn');
 const formWrapper = document.querySelector('.contact_form_wrapper');
 const form = document.querySelector('#form');
+const contactInfo = document.querySelector('.contact_info_container');
+const formContainer = document.querySelector('.form_container');
 
 mailBtn.addEventListener('click', () => {
   formWrapper.classList.toggle('active');
+  contactInfo.classList.toggle('active');
+  formContainer.classList.toggle('active');
 });
